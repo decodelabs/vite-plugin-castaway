a4539e134b8bf985d38c0e9cc2f11a32e14a6f12
Tom Wright <tom@inflatablecookie.com>
2025-04-08 07:36:45 +0000

    Updated structure
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- src/index.ts
+++ src/index.ts
@@ -8,63 +8,65 @@ import {
     type OutputOptions
 } from 'rollup';
 
-export default (options): Plugin => ({
-    name: 'vite:castaway',
+export default (): Plugin => {
+    return {
+        name: 'vite:castaway',
 
-    config: (
-        config: UserConfig
-    ) => {
-        const output = config.build?.rollupOptions?.output as OutputOptions | undefined;
-        let existing = output?.manualChunks ?? undefined;
+        config: (
+            config: UserConfig
+        ) => {
+            const output = config.build?.rollupOptions?.output as OutputOptions | undefined;
+            let existing = output?.manualChunks ?? undefined;
 
-        if (typeof existing !== 'function') {
-            existing = undefined;
-        }
+            if (typeof existing !== 'function') {
+                existing = undefined;
+            }
 
-        return {
-            build: {
-                rollupOptions: {
-                    output: {
-                        manualChunks: (
-                            id: string,
-                            meta: ManualChunkMeta
-                        ) => {
-                            // React
-                            if (
-                                id.includes('node_modules/react') ||
-                                id.endsWith('integrations/react.ts')
-                            ) {
-                                return 'react';
-                            }
+            return {
+                build: {
+                    rollupOptions: {
+                        output: {
+                            manualChunks: (
+                                id: string,
+                                meta: ManualChunkMeta
+                            ) => {
+                                // React
+                                if (
+                                    id.includes('node_modules/react') ||
+                                    id.endsWith('integrations/react.ts')
+                                ) {
+                                    return 'react';
+                                }
 
-                            // Vue
-                            if (
-                                id.includes('@vue') ||
-                                id.includes('node_modules/vue/dist') ||
-                                id.endsWith('integrations/vue.ts')
-                            ) {
-                                return 'vue';
-                            }
+                                // Vue
+                                if (
+                                    id.includes('@vue') ||
+                                    id.includes('node_modules/vue/dist') ||
+                                    id.endsWith('integrations/vue.ts')
+                                ) {
+                                    return 'vue';
+                                }
 
-                            if (existing) {
-                                const output = existing(id, meta);
+                                if (existing) {
+                                    const output = existing(id, meta);
 
-                                if (output !== undefined) {
-                                    return output;
+                                    if (output !== undefined) {
+                                        return output;
+                                    }
+                                }
+
+                                if (
+                                    id.endsWith('.vue') ||
+                                    id.endsWith('.jsx')
+                                ) {
+                                    return 'components';
                                 }
                             }
-
-                            if (
-                                id.endsWith('.vue') ||
-                                id.endsWith('.jsx')
-                            ) {
-                                return 'components';
-                            }
-                        }
-                    },
-                    preserveEntrySignatures: 'strict'
+                        },
+                        preserveEntrySignatures: 'strict'
+                    }
                 }
             }
         }
     }
-});
+};
